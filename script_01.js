08b62907873b7c0e5c4d75bbd4af8b8cbe8b48d8
Serhedo <serhat.alpayci@gmx.de>
2023-05-12 10:44:20 +0200

    module: divide a / b | 1. Iteration

--- script_01.js
+++ script_01.js
@@ -11,10 +11,35 @@
 4. Ausgabe in Konsole : check!
 */
 
+
+// module: divide a / b |  test:
+output(divide(4,2));
+output(divide(3,2));
+output(divide(3,-2));
+output(divide(3,0));
+function divide(a,b) {
+
+	if (b!=0) {
+		return a/b;
+	} else {
+		return "Teilen durch 0 nicht möglich";
+	}
+	return a/b;
+}
+
+
+// module: multiplication a * b |  test:
+// output(multiply(3,2));
+// output(multiply(3,-2));
+// output(multiply(3,0));
+function multiply(a,b) {
+	return a*b;
+}
+
 // module: subtraction a - b |  test:
-output(subtract(3,2));
-output(subtract(3,-2));
-output(subtract(3,0));
+// output(subtract(3,2));
+// output(subtract(3,-2));
+// output(subtract(3,0));
 function subtract(a,b) {
 	return a-b;
 }
