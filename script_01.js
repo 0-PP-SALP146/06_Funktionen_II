bf068c719416723c488166d83991c631baca2e5c
Serhedo <serhat.alpayci@gmx.de>
2023-05-12 10:14:31 +0200

    module: addition a + b

--- script_01.js
+++ script_01.js
@@ -8,11 +8,17 @@
 1. Dateneingabe + -überprüfung : 
 2. Auswahl Rechenart : 
 3. Fkt. Grundrechenarten : 
-4. Ausgabe in Konsole : 
+4. Ausgabe in Konsole : check!
 */
 
 
-
+// module: addition a + b |  test:
+// output(add(2,2));
+// output(add(2,-2));
+// output(add(2,0));
+function add(a,b) {
+	return a + b;
+}
 
 
 // module: output | test:
@@ -22,3 +28,4 @@ function output(outputData) {
 	console.log(outputData);
 }
   
+
